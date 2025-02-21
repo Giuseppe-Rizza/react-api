7fbb6d2360061e0b851355c8bbd28ed45d4d3076
Giuseppe Rizza <ilmoro86@hotmail.it>
2025-02-21 09:58:33 +0100

    [feat] Add final step

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
+import axios from 'axios';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [postsList, setPostsList] = useState([]);
+
+  function fetchTodos() {
+    axios.get("http://localhost:3000/posts/")
+      .then((res) => setPostsList(res.data))
+  }
+
+  useEffect(fetchTodos, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      {postsList.length === 0 ? (
+        <h1>No posts</h1>) : (
+        postsList.map((post) => (
+          <div className='box-form' key={post.id}>
+            <h2>{post.title}</h2>
+            <img src={post.image} alt={post.title} />
+            <p>{post.content}</p>
+            <p>{post.tags.join(", ")}</p>
+            <button onClick={() => removePost(post.id)}>Delete</button>
+          </div >
+        ))
+      )}
     </>
   )
 }
